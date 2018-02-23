f3092edec1ffbcf8e159186e9621d899ff104f1a
Kacey Cleveland <k1996@ad.unc.edu>
2018-02-23 16:32:39 -0500

    navigation to how to play

---
+++ Coastal Defenders Unity/Assets/InitialPlayButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InitialPlayButton : MonoBehaviour {
+
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("howtoplay");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
