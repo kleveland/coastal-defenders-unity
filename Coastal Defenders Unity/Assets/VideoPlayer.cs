3b4f6ff17c9483ff2cd90a0fa4c4b546c290c317
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2018-03-02 16:45:02 -0500

    updated how to play, needs to be polished and fixed.

---
+++ Coastal Defenders Unity/Assets/VideoPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject camera = GameObject.Find("Main Camera");
+        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+        videoPlayer.isLooping = true;
+        videoPlayer.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
