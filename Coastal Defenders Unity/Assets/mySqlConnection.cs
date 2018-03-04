be6440685195ba57d6216e40a62b78f3cbf84b04
Kacey Cleveland <k1996@ad.unc.edu>
2018-03-04 18:37:45 -0500

    added basic database communication

---
+++ Coastal Defenders Unity/Assets/mySqlConnection.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mySqlConnection : MonoBehaviour {
+
+    // Use this for initialization
+    public string url = "localhost:4000/leaderboard/scores";
+    public ScoreEntry[] scoreEntries;
+    public Button yourButton;
+
+    void Start () {
+        yourButton.onClick.AddListener(outputScores);
+        StartCoroutine(getScores());
+    }
+
+    void outputScores()
+    {
+        for(int i=0; i<scoreEntries.Length; i++)
+        {
+            Debug.Log(i + ": " + scoreEntries[i].name + ": " + scoreEntries[i].score);
+        }
+    }
+
+    IEnumerator getScores()
+    {
+        using (WWW www = new WWW(url))
+        {
+            yield return www;
+            scoreEntries = JsonHelper.FromJson<ScoreEntry>(www.text);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
+
+public static class JsonHelper
+{
+    public static T[] FromJson<T>(string json)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.Items;
+    }
+
+    public static string ToJson<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Items = array;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    public static string ToJson<T>(T[] array, bool prettyPrint)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Items = array;
+        return JsonUtility.ToJson(wrapper, prettyPrint);
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] Items;
+    }
+}
+
+[System.Serializable]
+public class ScoreEntry
+{
+    public int id = 0;
+    public string name = "test";
+    public int score = 1;
+}
