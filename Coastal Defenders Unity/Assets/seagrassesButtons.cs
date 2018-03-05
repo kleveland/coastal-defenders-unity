277e9bc025dc3bbe346df99148717e77db7661b2
Joey Chau <joeychau@live.unc.edu>
2018-03-04 23:30:46 -0500

    Added C# counters to +/- buttons.

--- Coastal Defenders Unity/Assets/seagrassesButtons.cs
+++ Coastal Defenders Unity/Assets/seagrassesButtons.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+
+[RequireComponent(typeof(Button))]
 public class seagrassesButtons : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Button plusButton;
+    public Button minusButton;
+    public Sprite[] seagrasses;
+    private int count = 0;
+
+    void Awake()
+    {
+        Button plusbtn = plusButton.GetComponent<Button>();
+        Button minusbtn = minusButton.GetComponent<Button>();
+        plusbtn.onClick.AddListener(PlusTaskOnClick);
+        minusbtn.onClick.AddListener(MinusTaskOnClick);
+
+    }
+
+    void PlusTaskOnClick()
+    {
+        Debug.Log("Clicked plus button");
+        count++;
+        if (count >= seagrasses.Length)
+        {
+            count = 3;
+        }
+
+        Debug.Log(count);
+
+    }
+    void MinusTaskOnClick()
+    {
+        Debug.Log("Clicked minus button");
+        count--;
+        if (count < 0)
+        {
+            count = 0;
+            return;
+        }
+
+        Debug.Log(count);
+    }
 }
