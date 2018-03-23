3ad32a1f4b037d37de0b90b989795151b62aa91e
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2018-03-23 16:34:53 -0400

    added countdown timer, started on point script its buggy.

---
+++ Coastal Defenders Unity/Assets/Timer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public int timeLeft = 180;
+    public Text countdownText;
+    public string navToScene;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownText.text = ("Time: " + timeLeft);
+
+        if (timeLeft <= 0)
+        {
+            StopCoroutine("LoseTime");
+            countdownText.text = "Times Up!";
+            SceneManager.LoadScene(navToScene);
+        }
+    }
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+}
