2b078210b0d7375ae890a9543b54e74dc4bdfeb2
Kacey Cleveland <k1996@ad.unc.edu>
2018-04-19 15:40:46 -0400

    demo #2

--- Coastal Defenders Unity/Assets/StartRun.cs
+++ Coastal Defenders Unity/Assets/StartRun.cs
@@ -6,7 +6,7 @@ public class StartRun : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Screen.SetResolution(540, 960, false);
+        //Screen.SetResolution(540, 960, false);
     }
 	
 	// Update is called once per frame
