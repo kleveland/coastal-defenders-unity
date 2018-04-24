cf803ae7e302dafa2f506bc1d2367cedffa84c39
Kacey Cleveland <k1996@ad.unc.edu>
2018-04-24 07:12:22 -0400

    keyboard progress

---
+++ Coastal Defenders Unity/Assets/KeyboardController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour {
+
+    private KeyboardKey[] keys;
+    private string output;
+
+	// Use this for initialization
+	void Start () {
+        output = "";
+        keys = this.GetComponentsInChildren<KeyboardKey>();
+        Debug.Log(keys[0].but);
+        for(int i=0; i<keys.Length; i++)
+        {
+            Debug.Log(keys[i].character);
+            string tempStr = keys[i].character;
+            keys[i].but.onClick.AddListener(() => { onClickOutput(tempStr); });
+            Debug.Log(keys[i].character);
+        }
+	}
+
+    void onClickOutput(string val)
+    {
+        if(val.Equals("Back"))
+        {
+            output = output.Substring(0, output.Length - 1);
+        } else if(val.Equals("Enter"))
+        {
+
+        } else
+        {
+            output += val;
+        }
+        Debug.Log(output);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Coastal Defenders Unity/Assets/KeyboardKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardKey : MonoBehaviour {
+
+    public string character;
+    public Button but;
+
+
+	// Use this for initialization
+	void Awake () {
+        this.GetComponentInChildren<Text>().text = character;
+        but = (Button) this.GetComponent("Button");
+        Debug.Log(this.GetComponent<Button>());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
