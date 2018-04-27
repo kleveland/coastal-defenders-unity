bed89fb83d7823ac7e8e7f4ed42b08492b5d7faa
Sriganesh Gopal <sriganesh.iswar@gmail.com>
2018-04-27 16:52:02 -0400

    Working on Sub Effectiveness Bars on End Game Screen.

--- Coastal Defenders Unity/Assets/ScoreCalculator.cs
+++ Coastal Defenders Unity/Assets/ScoreCalculator.cs
@@ -8,23 +8,29 @@ public class ScoreCalculator : MonoBehaviour
 {
     public Text netScoreText;
     public Text subScoreText;
-    private float netScore;
-    private float netLandScore;
-    private float netAnimalScore;
-    private float netHumanScore;
+    public int[] defenseThresholds_1;
+    public int[] defenseThresholds_2;
+    public int[] defenseThresholds_3;
+    public Image humanBar;
+    public Image propertyBar;
+    public Image ecosystemBar;
+    private int netScore;
+    private int netLandScore;
+    private int netAnimalScore;
+    private int netHumanScore;
+
     // Use this for initialization
     void Awake()
     {
-        netLandScore = 620f * Mathf.Log10(PlayerPrefs.GetFloat("netLand") + 1);
-        netAnimalScore = 620 * Mathf.Log10(PlayerPrefs.GetFloat("netAnimal") + 1);
-        netHumanScore = 620f * Mathf.Log10(PlayerPrefs.GetFloat("netHuman") + 1);
+        netLandScore = Mathf.RoundToInt(620f * Mathf.Log10(PlayerPrefs.GetFloat("netLand") + 1));
+        netAnimalScore = Mathf.RoundToInt(620 * Mathf.Log10(PlayerPrefs.GetFloat("netAnimal") + 1));
+        netHumanScore = Mathf.RoundToInt(620f * Mathf.Log10(PlayerPrefs.GetFloat("netHuman") + 1));
         netScore = netLandScore + netHumanScore + netAnimalScore;
-
-        PlayerPrefs.SetFloat("netScore", Mathf.RoundToInt(netScore));
-        netScoreText.text = "Your Score\n<size=86> " + Mathf.RoundToInt(netScore)+"</size>";
+        PlayerPrefs.SetFloat("netScore", netScore);
+        netScoreText.text = "Your Score\n<size=86> " + Mathf.RoundToInt(netScore) + "</size>";
 
         subScoreText.text = "Land Score: " + Mathf.RoundToInt(netLandScore) + "\nHuman Score: " + Mathf.RoundToInt(netHumanScore) + "\nAnimal Score: " + Mathf.RoundToInt(netAnimalScore);
-
+        //humanBar.
 
     }
 
