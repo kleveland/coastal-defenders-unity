c15b161cb6889de0ccab3375fa06ecdf40d21a35
Kacey Cleveland <k1996@ad.unc.edu>
2018-05-06 03:12:04 -0400

    learn more fix and bar fix

--- Coastal Defenders Unity/Assets/ScoreCalculator.cs
+++ Coastal Defenders Unity/Assets/ScoreCalculator.cs
@@ -13,9 +13,9 @@ public class ScoreCalculator : MonoBehaviour
     public int[] defenseThresholds_1;
     public int[] defenseThresholds_2;
     public int[] defenseThresholds_3;
-    public Image humanBar;
-    public Image propertyBar;
-    public Image ecosystemBar;
+    public RawImage humanBar;
+    public RawImage propertyBar;
+    public RawImage ecosystemBar;
     public static int netScore;
     private int netLandScore;
     private int netAnimalScore;
@@ -30,25 +30,72 @@ public class ScoreCalculator : MonoBehaviour
         netScore = netLandScore + netHumanScore + netAnimalScore;
         PlayerPrefs.SetFloat("netScore", netScore);
         netScoreText.text = Mathf.RoundToInt(netScore).ToString();
+        float humanpercent = 0;
+        float landpercent = 0;
+        float animalpercent = 0;
         if(DiffController.levelselect == 1)
         {
+            humanpercent = (PlayerPrefs.GetFloat("netHuman") / 20);
+            landpercent = (PlayerPrefs.GetFloat("netLand") / 20);
+            animalpercent = (PlayerPrefs.GetFloat("netAnimal") / 20);
             humanPBar.offsetMax = new Vector2((float)(-( 207.5 - 315*(PlayerPrefs.GetFloat("netHuman") / 20))), humanPBar.offsetMax.y);
             propertyPBar.offsetMax = new Vector2((float)(-( 207.5 - 315 * (PlayerPrefs.GetFloat("netLand") / 20))), propertyPBar.offsetMax.y);
             ecoPBar.offsetMax = new Vector2((float)(-( 207.5 - 315 * (PlayerPrefs.GetFloat("netAnimal") / 20))), ecoPBar.offsetMax.y);
         } else if(DiffController.levelselect == 2)
         {
+            humanpercent = (PlayerPrefs.GetFloat("netHuman") / 30);
+            landpercent = (PlayerPrefs.GetFloat("netLand") / 25);
+            animalpercent = (PlayerPrefs.GetFloat("netAnimal") / 25);
             humanPBar.offsetMax = new Vector2((float)(-( 207.5 - 315 * (PlayerPrefs.GetFloat("netHuman") / 30))), humanPBar.offsetMax.y);
             propertyPBar.offsetMax = new Vector2((float)(-( 207.5 - 315 * (PlayerPrefs.GetFloat("netLand") / 25))), propertyPBar.offsetMax.y);
             ecoPBar.offsetMax = new Vector2((float)(-( 207.5 - 315 * (PlayerPrefs.GetFloat("netAnimal") / 25))), ecoPBar.offsetMax.y);
 
         } else if(DiffController.levelselect == 3)
         {
+            humanpercent = (PlayerPrefs.GetFloat("netHuman") / 15);
+            landpercent = (PlayerPrefs.GetFloat("netLand") / 35);
+            animalpercent = (PlayerPrefs.GetFloat("netAnimal") / 25);
             humanPBar.offsetMax = new Vector2((float)(-( 207.5 - 315 * (PlayerPrefs.GetFloat("netHuman") / 15))), humanPBar.offsetMax.y);
             propertyPBar.offsetMax = new Vector2((float)(-( 207.5 - 315 * (PlayerPrefs.GetFloat("netLand") / 35))), propertyPBar.offsetMax.y);
             ecoPBar.offsetMax = new Vector2((float)(-( 207.5 - 315 * (PlayerPrefs.GetFloat("netAnimal") / 25))), ecoPBar.offsetMax.y);
         }
 
-        if(-humanPBar.offsetMax.x < -107.5)
+        if(humanpercent < 0.3)
+        {
+            humanBar.color = Color.red;
+        } else if(humanpercent < 0.7)
+        {
+            humanBar.color = Color.yellow;
+        } else
+        {
+            humanBar.color = Color.green;
+        }
+        if (landpercent < 0.3)
+        {
+            propertyBar.color = Color.red;
+        }
+        else if (landpercent < 0.7)
+        {
+            propertyBar.color = Color.yellow;
+        }
+        else
+        {
+            propertyBar.color = Color.green;
+        }
+        if (animalpercent < 0.3)
+        {
+            ecosystemBar.color = Color.red;
+        }
+        else if (animalpercent < 0.7)
+        {
+            ecosystemBar.color = Color.yellow;
+        }
+        else
+        {
+            ecosystemBar.color = Color.green;
+        }
+
+        if (-humanPBar.offsetMax.x < -107.5)
         {
             humanPBar.offsetMax = new Vector2((float)-(-107.5), humanPBar.offsetMax.y);
         }
@@ -60,6 +107,7 @@ public class ScoreCalculator : MonoBehaviour
         {
             ecoPBar.offsetMax = new Vector2((float)-(-107.5), ecoPBar.offsetMax.y);
         }
+        
         Debug.Log("VALUES");
         Debug.Log((PlayerPrefs.GetFloat("netHuman")));
         Debug.Log((PlayerPrefs.GetFloat("netLand")));
